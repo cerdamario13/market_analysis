72d5ff76befc0976bf7f688ac49d6deb0ae02f81
Mario <cerdamario13@gmail.com>
2022-06-30 20:33:27 -0500

    added gitignore file

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import func
+
+# Testings
+dates = func.date_range_data('6/1/2022', '6/30/2022', 'AAPL')
+print(dates)
+
+
+# TODO Add ago functions
+# 5 days, 6 months, year to date, 1 year, 5 year, max
