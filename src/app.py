4ccd958e7599d9b6f1e2891dae09f9e5c8780139
Mario <cerdamario13@gmail.com>
2022-07-09 22:09:53 -0500

    added x time ago to return

--- src/app.py
+++ src/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import func
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello_world():
     return 'Hello World!'
     
+@app.route("/time2_ago", methods=['POST'])
+def time2_ago():
+    
+    # Loading initial data
+    ticker = request.args.get('ticker')
+    data = func.ticker_data_all(ticker)
+    
+    time_frame = request.args.get('time')
+    data_info = request.args.get('data_info')
+    
+    # Getting data from x time ago
+    results = func.x_month_ago(data, int(time_frame), data_info)
+    
+    return jsonify(results.tolist())
     
 if __name__ == '__main__':
     app.run(debug=False, port=8000, host='0.0.0.0')
