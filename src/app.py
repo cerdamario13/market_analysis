e012654e9e7c3275399eda6fe6a4b2fabdaecd9c
Mario <cerdamario13@gmail.com>
2022-07-11 21:24:46 -0500

    more front end

--- src/app.py
+++ src/app.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, redirect, render_template, request, jsonify, url_for
 import func
+import plotly
+import plotly.express as px
+import json
 
 app = Flask(__name__)
 
-@app.route("/home", methods=['GET'])
+@app.route("/", methods=['POST', 'GET'])
+@app.route("/home", methods=['POST', 'GET'])
 def hello_world():
-    return render_template('home.html')
+    
+    if request.method == 'POST':
+            
+        # Loading initial data
+        ticker = request.form['ticker']
+        time_frame = request.form['time']
+        data_info = request.form['data_info']
+        data = func.ticker_data_all(ticker)
+        # Getting data from x time ago
+        results = func.x_month_ago(data, int(time_frame), data_info)
+        # return jsonify(results.tolist())
         
-@app.route("/time2_ago", methods=['POST'])
-def time2_ago():
-    
-    # Loading initial data
-    ticker = request.args.get('ticker')
-    data = func.ticker_data_all(ticker)
-    
-    time_frame = request.args.get('time')
-    data_info = request.args.get('data_info')
-    
-    # Getting data from x time ago
-    results = func.x_month_ago(data, int(time_frame), data_info)
-    
-    return jsonify(results.tolist())
+        fig = px.bar(results)
+        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        
+        
+        return render_template('base.html', graphJSON=graphJSON)
+    else:
+        return render_template('base.html')
+        
+@app.route("/new")
+def test_new():
+    return render_template('new.html')
     
 if __name__ == '__main__':
-    app.run(debug=False, port=8000, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
